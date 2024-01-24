496d819a8bb5586d2e1a4b3445e8d7d69554ad65
Brittany Francis <brittanyfrancis200@gmail.com>
2024-01-24 13:19:51 -0500

    userdashboard

---
+++ predictions/client/src/components/userdashboard/DashboardLeft.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const DashboardLeft = () => {
+  return (
+    <div>
+        {/* This will contain links to each of the following */}
+        Global Leaderboard
+        Predictions
+        Your Leagues
+        Create a League
+        <form action="">
+            {/* create league form */}
+        </form>
+    </div>
+  )
+}
+
+export default DashboardLeft
---
+++ predictions/client/src/components/userdashboard/DashboardRight.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const DashboardRight = () => {
+  return (
+    <div>
+        {/* axios call to get available leagues
+        links for leagues user can join */}
+    </div>
+  )
+}
+
+export default DashboardRight
