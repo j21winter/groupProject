8decdd5a13305ebb2985fe0a9b655545543f4aa9
j21winter <j21winter@gmail.com>
2024-01-29 00:19:55 -0600

    dahsboard notes

--- predictions/client/src/components/userdashboard/DashboardMid.jsx
+++ predictions/client/src/components/userdashboard/DashboardMid.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import UserContext from '../../context/userContext'
 
 export const DashboardMid = () => {
+  const {user, scoresAndPredictions} = useContext(UserContext)
+
  {/* api calls to get info from Games
         axios call to get your points */}
         
 
   return (
     <div>
+        <p>Your points : {user.points}</p>
         <p>Recent Games</p>
+        {/* go through scores and predictions and display the game weeks that have PREVIOUS/CURRENT/NEXT as true statements */}
         <p>Upcoming Games</p>
-        <p>Your points</p>
+
 
     </div>
   )
