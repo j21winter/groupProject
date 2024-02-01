757cf3cd7e38a48a419822ab4d216f65be6f3b4f
Phone Myat <hanminsoe10@gmail.com>
2024-02-01 02:22:07 -0600

    do protected routes

--- predictions/client/src/components/protected/ProtectedRoute.jsx
+++ predictions/client/src/components/protected/ProtectedRoute.jsx
@@ -1,26 +1,18 @@
-import React, {useEffect, useContext} from 'react'
+import React, { useEffect, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import UserContext from '../../context/userContext';
 
-
-
-export default function ProtectedRoute({children}) {
+export default function ProtectedRoute({ children }) {
     const navigate = useNavigate();
-    //to check user if its empty from user context
-    const {user} = useContext(UserContext)
-    // console.log('this is the user: ' + user)
-    
-    // conditon to check if object is empty
-    function isEmpty(obj) {
-        return obj && Object.keys(obj).length === 0 && obj.constructor === Object;
-    }
-    
-    useEffect(() => {
-        // check if user is not present => to Homepage
-    if (isEmpty(user)) {
-        navigate('/');
-    }
-    }, [user]);
+    const { user } = useContext(UserContext);
 
-    return children // If user is present continue with the children component
+    // check if userContext is empty if yes => Homepage
+    useEffect(() => {
+        if (!user || Object.keys(user).length === 0) {
+            navigate('/');
+        }
+    }, [user, navigate]);
+
+    // if there is user take to children component if not null
+    return user && Object.keys(user).length > 0 ? children : null;
 }
